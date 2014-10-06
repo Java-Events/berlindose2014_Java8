2f11ec084d33db7d57f6b07273a1e91606200936
Christopher Ewest <christopher.ewest@tolina.de>
2014-10-06 11:28:37 +0200

    M_*

--- src/main/java/org/rapidpm/course/java8/jsr310/M_TemporalAdjuster.java
+++ src/main/java/org/rapidpm/course/java8/jsr310/M_TemporalAdjuster.java
@@ -3,6 +3,7 @@ package org.rapidpm.course.java8.jsr310;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
@@ -12,56 +13,56 @@ import java.time.temporal.TemporalAdjusters;
  */
 public class M_TemporalAdjuster {
 
-    final String demoString = "sssss";
+	final String demoString = "sssss";
 
-    final static TemporalAdjuster adjuster = temporal -> {
-        final LocalDate localDate = LocalDate.from(temporal);
-        final DayOfWeek dayOfWeek = localDate.getDayOfWeek();
-        if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
-            final TemporalAdjuster next = TemporalAdjusters.next(DayOfWeek.MONDAY);
-            return temporal.with(next);
-        } else {
-            return temporal.with(localDate);
-        }
-    };
+	//    final static TemporalAdjuster adjuster = temporal -> {
+	//        final LocalDate localDate = LocalDate.from(temporal);
+	//        final DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+	//        if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
+	//            final TemporalAdjuster next = TemporalAdjusters.next(DayOfWeek.MONDAY);
+	//            return temporal.with(next);
+	//        } else {
+	//            return temporal.with(localDate);
+	//        }
+	//    };
 
-    public static void main(String[] args) {
-        LocalDate date = LocalDate.of(2014, Month.APRIL, 3);
-        DayOfWeek dotw = date.getDayOfWeek();
-        System.out.printf("%s is on a %s%n", date, dotw);
-        System.out.printf("first day of Month: %s%n",
-                date.with(TemporalAdjusters.firstDayOfMonth()));
+	final static TemporalAdjuster adjuster = new TemporalAdjuster() {
 
-        final LocalDate d1 = date.with(new MyTemporalAdjuster());
-        System.out.println("d1 = " + d1);
+		@Override
+		public Temporal adjustInto(final Temporal temporal) {
+			return temporal;
+		}
+	};
 
+	public static void main(final String[] args) {
+		final LocalDate date = LocalDate.of(2014, Month.APRIL, 3);
+		final DayOfWeek dotw = date.getDayOfWeek();
+		System.out.printf("%s is on a %s%n", date, dotw);
+		System.out.printf("first day of Month: %s%n", date.with(TemporalAdjusters.firstDayOfMonth()));
 
-
-        final LocalDate d1x = date.with(adjuster);
-        System.out.println("d1x = " + d1x);
-
-        final LocalDate dateSa = LocalDate.of(2014, Month.APRIL, 5);
-        System.out.println("d2 = " + dateSa.with(new MyTemporalAdjuster()));
-
-    }
+		final LocalDate d1 = date.with(new MyTemporalAdjuster());
+		System.out.println("d1 = " + d1);
 
 
-    /**
-     * get next Monday if WeekEnd
-     */
-    public static class MyTemporalAdjuster implements TemporalAdjuster {
-        @Override
-        public Temporal adjustInto(Temporal temporal) {
-            final LocalDate localDate = LocalDate.from(temporal);
-            final DayOfWeek dayOfWeek = localDate.getDayOfWeek();
-            if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
-                final TemporalAdjuster next = TemporalAdjusters.next(DayOfWeek.MONDAY);
-                return temporal.with(next);
-            } else {
-                return temporal.with(localDate);
-            }
-        }
-    }
+
+		final LocalDate d1x = date.with(adjuster);
+		System.out.println("d1x = " + d1x);
+
+		final LocalDate dateSa = LocalDate.of(2014, Month.APRIL, 5);
+		System.out.println("d2 = " + dateSa.getDayOfWeek());
+		System.out.println("d2 = " + dateSa.with(new MyTemporalAdjuster()).getDayOfWeek());
+
+	}
 
 
+	/**
+	 * get next Monday if WeekEnd
+	 */
+	public static class MyTemporalAdjuster implements TemporalAdjuster {
+		@Override
+		public Temporal adjustInto(final Temporal temporal) {
+			return temporal.minus(15, ChronoUnit.CENTURIES);
+			//			return temporal;
+		}
+	}
 }
