2ff5364cb97981a354bce6d7fee9c7f9dfa5e32c
p06 <a.tappe@gmx.de>
2014-10-06 11:15:20 +0200

    Zoned... Experimente

--- src/main/java/org/rapidpm/course/java8/jsr310/p06/HelloWorld.java
+++ src/main/java/org/rapidpm/course/java8/jsr310/p06/HelloWorld.java
@@ -1,7 +1,15 @@
 package org.rapidpm.course.java8.jsr310.p06;
 
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Tag!");
+		ZonedDateTime berlin = ZonedDateTime.now();
+		ZonedDateTime london = ZonedDateTime.now(ZoneId.of("Europe/London"));
+		DateTimeFormatter pattern = DateTimeFormatter.ofPattern("VV: yyyy-MM-dd - hh:mm:ss");
+		System.out.println(berlin.format(pattern));
+		System.out.println(london.format(pattern));
 	}
 }
