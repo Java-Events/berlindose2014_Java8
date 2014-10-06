f699cd3d1ce0601c4674d90ced77b1b091b1a0b7
Daniel Kränkle <daniel.kraenkle@tolina.de>
2014-10-06 13:10:25 +0200

    offset

---
+++ src/main/java/org/rapidpm/course/java8/jsr310/p07/Aufgabe_OffsetDateTime.java
@@ -0,0 +1,31 @@
+package org.rapidpm.course.java8.jsr310.p07;
+
+import java.time.*;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalAdjusters;
+
+/**
+ *
+ * Ersetze die Angabe -02:00 durch die ZeitZone Europe/Berlin
+ *
+ *
+ * Created by Sven Ruppert on 31.03.2014.
+ */
+public class Aufgabe_OffsetDateTime {
+	public static void main(String[] args) {
+		LocalDateTime date = LocalDateTime.of(2014, Month.APRIL, 20, 19, 30);
+		
+		// ZoneOffset offset = ZoneOffset.of("-02:00");
+		
+		// Offset Berlin zu UTC
+		ZoneId zoneIdBerlin = ZoneId.of("Europe/Berlin");
+		ZonedDateTime timeInBerlin = date.atZone(zoneIdBerlin);
+		final ZoneOffset offset = timeInBerlin.getOffset();
+
+		OffsetDateTime dateOffset = OffsetDateTime.of(date, offset);
+		OffsetDateTime lastThursday = dateOffset.with(TemporalAdjusters
+				.lastInMonth(DayOfWeek.THURSDAY));
+		System.out.printf("The last Thursday in April 2014 is the %sth.%n",
+				lastThursday.getDayOfMonth());
+	}
+}
