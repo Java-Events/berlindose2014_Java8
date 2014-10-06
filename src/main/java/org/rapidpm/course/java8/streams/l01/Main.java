8d66f748c3bbab6766adc62abe97a2a5403f9efc
Daniel Kränkle <daniel.kraenkle@tolina.de>
2014-10-06 15:35:00 +0200

    streams

--- src/main/java/org/rapidpm/course/java8/streams/l01/Main.java
+++ src/main/java/org/rapidpm/course/java8/streams/l01/Main.java
@@ -1,22 +1,37 @@
 package org.rapidpm.course.java8.streams.l01;
 
-import org.rapidpm.course.java8.streams.model.Pair;
-
 import java.util.List;
+import java.util.stream.Stream;
+
+import org.rapidpm.course.java8.streams.model.Pair;
 
 /**
  * Created by Sven Ruppert on 06.10.2014.
  */
 public class Main {
 
-  public static void main(String[] args) {
-    DemoValueGenerator  generator = new DemoValueGenerator();
+	public static void main(String[] args) {
+		DemoValueGenerator generator = new DemoValueGenerator();
 
-    final List<Pair> generate = generator.generate();
+		final List<Pair> generatedList = generator.generate();
+		
+		Stream<Pair> stream1 = generatedList.stream();
+//	    Stream<Pair> stream2 = Stream.<Pair>builder().add(new Pair.Builder().counter(1).txt("txt").build()).build();
+//		Stream<Pair> stream3 = Stream.generate(() ->  new Pair.Builder().counter(1).txt("txt").build());
+		
+//		stream1.forEachOrdered(p -> System.out.println(p.getCounter() +": "+p.getTxt()));
+//		stream1.forEachOrdered(System.out::println);
+		
+//		Pair[] array = stream1.toArray(Pair[]::new);
+//		Set<Pair> collectedToSet = stream1.collect(Collectors.toSet());
+//		Map<Integer, List<Pair>> collectedToMap = stream1.collect(Collectors.groupingBy(Pair::getCounter));
+		
+//		Stream<String> pairTxtStream = stream1.map(p -> {
+//			return p.getTxt();
+//		});
+		
+		Stream<String> pairTxtStream = stream1.map(p -> p.getTxt());
 
-
-
-  }
-
+	}
 
 }
