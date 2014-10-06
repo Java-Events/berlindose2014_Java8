8c1d9a974367eb2a95fab73dd3398b407a7bb11e
Christopher Ewest <christopher.ewest@tolina.de>
2014-10-06 15:34:53 +0200

    map

--- src/main/java/org/rapidpm/course/java8/streams/l01/p08/Main.java
+++ src/main/java/org/rapidpm/course/java8/streams/l01/p08/Main.java
@@ -1,8 +1,5 @@
 package org.rapidpm.course.java8.streams.l01.p08;
 
-import java.util.List;
-import java.util.stream.Stream;
-
 import org.rapidpm.course.java8.streams.l01.DemoValueGenerator;
 import org.rapidpm.course.java8.streams.model.Pair;
 
@@ -13,14 +10,18 @@ public class Main {
 
 	public static void main(final String[] args) {
 		final DemoValueGenerator generator = new DemoValueGenerator();
+		//		final Stream<Pair> stream = generator.generate().stream();
+		//		final Stream<Pair> of = Stream.of(new Pair.Builder().counter(1).txt("A").build());
+		//		final Stream<Pair> add = Stream.<Pair> builder().add(new Pair.Builder().counter(1).txt("A").build()).build();
+		//		final Stream<Pair> generate = Stream.<Pair> generate(() -> new Pair.Builder().counter(1).txt("A").build());
+		//
+		//		final Map<Integer, List<Pair>> collect = stream.collect(Collectors.groupingBy(Pair::getCounter));
+		//		final Set<Pair> collect2 = of.collect(Collectors.toSet());
+		//		final List<Pair> collect3 = add.collect(Collectors.toList());
+		//		final Pair[] array = generate.toArray(Pair[]::new);
 
-		final List<Pair> generate = generator.generate();
-		final Stream<Pair> stream1 = generate.stream();
-		final Stream<Pair> stream2 = Stream.of(new Pair.Builder().counter(1).txt("A").build());
-		final Stream<Pair> stream3 = Stream.<Pair> builder().add(new Pair.Builder().counter(1).txt("A").build()).build();
-		final Stream<Pair> stream4 = Stream.<Pair> generate(() -> new Pair.Builder().counter(1).txt("A").build());
-
+		/*final Stream<String> map = */generator.generate().stream().map(Pair::getTxt).forEach(System.out::println);
+		;
 	}
-
 
 }
