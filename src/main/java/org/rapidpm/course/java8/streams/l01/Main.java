65c43d01444a3c2976abfbc04002cf5409a2f2bd
Daniel Kränkle <daniel.kraenkle@tolina.de>
2014-10-06 15:38:52 +0200

    -

--- src/main/java/org/rapidpm/course/java8/streams/l01/Main.java
+++ src/main/java/org/rapidpm/course/java8/streams/l01/Main.java
@@ -14,23 +14,6 @@ public class Main {
 		DemoValueGenerator generator = new DemoValueGenerator();
 
 		final List<Pair> generatedList = generator.generate();
-		
-		Stream<Pair> stream1 = generatedList.stream();
-//	    Stream<Pair> stream2 = Stream.<Pair>builder().add(new Pair.Builder().counter(1).txt("txt").build()).build();
-//		Stream<Pair> stream3 = Stream.generate(() ->  new Pair.Builder().counter(1).txt("txt").build());
-		
-//		stream1.forEachOrdered(p -> System.out.println(p.getCounter() +": "+p.getTxt()));
-//		stream1.forEachOrdered(System.out::println);
-		
-//		Pair[] array = stream1.toArray(Pair[]::new);
-//		Set<Pair> collectedToSet = stream1.collect(Collectors.toSet());
-//		Map<Integer, List<Pair>> collectedToMap = stream1.collect(Collectors.groupingBy(Pair::getCounter));
-		
-//		Stream<String> pairTxtStream = stream1.map(p -> {
-//			return p.getTxt();
-//		});
-		
-		Stream<String> pairTxtStream = stream1.map(p -> p.getTxt());
 
 	}
 
